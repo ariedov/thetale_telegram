67fcb52c6141b8e390b43dd7456a7fefa5747476
david <ariedov@gmail.com>
2018-10-30 23:13:03 +0200

    initial commit

---
+++ bin/main.dart
@@ -0,0 +1,13 @@
+import 'package:epictale_telegram/epictale_telegram.dart';
+
+Future main() async {
+  final app = Application<EpictaleTelegramChannel>()
+      ..options.configurationFilePath = "config.yaml"
+      ..options.port = 8888;
+
+  final count = Platform.numberOfProcessors ~/ 2;
+  await app.start(numberOfInstances: count > 0 ? count : 1);
+
+  print("Application started on port: ${app.options.port}.");
+  print("Use Ctrl-C (SIGINT) to stop running the application.");
+}
---
+++ lib/epictale_telegram.dart
@@ -0,0 +1,11 @@
+/// epictale_telegram
+///
+/// A Aqueduct web server.
+library epictale_telegram;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:aqueduct/aqueduct.dart';
+
+export 'channel.dart';
