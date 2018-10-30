fdc8b2ca4aae55cc0301ae5a2d144c2fb6e7f88f
david <ariedov@gmail.com>
2018-10-30 23:15:59 +0200

    adds migration files

---
+++ migrations/00000001_initial.migration.dart
@@ -0,0 +1,17 @@
+import 'dart:async';
+
+import 'package:aqueduct/aqueduct.dart';   
+
+class Migration1 extends Migration { 
+  @override
+  Future upgrade() async {
+   
+  }
+  
+  @override
+  Future downgrade() async {}
+  
+  @override
+  Future seed() async {}
+}
+    
