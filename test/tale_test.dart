6a86335199af945fa7f8d5f597b10847924c90c5
david <ariedov@gmail.com>
2018-11-02 23:18:56 +0200

    adds third party test

--- test/tale_test.dart
+++ test/tale_test.dart
@@ -35,8 +35,8 @@ void main() {
           "game_version": "v0.3.27.1",
           "static_content": "//static.the-tale.org/static/247/",
           "turn_delta": 10
-      }
-    }""";
+        }
+      }""";
 
     final payloadJson = json.decode(payload);
     final response = convertResponse(payloadJson, convertApiInfo);
@@ -45,16 +45,30 @@ void main() {
     expect(response.data.gameVersion, "v0.3.27.1");
   });
 
+  test("test third party", () {
+    const payload = """{
+      "status": "ok",
+      "data": {
+          "authorisation_page": "/accounts/third-party/tokens/41b21f95-8b4f-41f0-acef-e5ca06edcaf0"
+      }
+    }""";
+
+    final payloadJson = json.decode(payload);
+    final response = convertResponse(payloadJson, convertThirdPartyLink);
+
+    expect(response.isError, false);
+    expect(response.data.authorizationPage,
+        "/accounts/third-party/tokens/41b21f95-8b4f-41f0-acef-e5ca06edcaf0");
+  });
+
   test("test headers read", () {
-    const payload =
-        """
-        sessionid=csxqqjj7cyiy9b3mukhzor9z9we9twks; expires=Fri, 16-Nov-2018 16:15:22 GMT; HttpOnly; Max-Age=1209600; Path=/; Secure,csrftoken=PVw6ZDIEt2UfcxsWwtdAES9Mwlq7FH7bxoyJMw1YlIj7wc3WhB0rKt6aRhzOkrOk; expires=Fri, 01-Nov-2019 16:15:22 GMT; HttpOnly; Max-Age=31449600; Path=/; Secure
-        }""";
+    const payload = """
+        sessionid=csxqqjj7cyiy9b3mukhzor9z9we9twks; expires=Fri, 16-Nov-2018 16:15:22 GMT; HttpOnly; Max-Age=1209600; Path=/; Secure,csrftoken=PVw6ZDIEt2UfcxsWwtdAES9Mwlq7FH7bxoyJMw1YlIj7wc3WhB0rKt6aRhzOkrOk; expires=Fri, 01-Nov-2019 16:15:22 GMT; HttpOnly; Max-Age=31449600; Path=/; Secure""";
 
     final session = readSessionInfo(payload);
 
     expect(session.sessionId, "csxqqjj7cyiy9b3mukhzor9z9we9twks");
-    expect(session.csrfToken, "PVw6ZDIEt2UfcxsWwtdAES9Mwlq7FH7bxoyJMw1YlIj7wc3WhB0rKt6aRhzOkrOk");
-
+    expect(session.csrfToken,
+        "PVw6ZDIEt2UfcxsWwtdAES9Mwlq7FH7bxoyJMw1YlIj7wc3WhB0rKt6aRhzOkrOk");
   });
 }
