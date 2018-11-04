7c38e538cfa59f64f4fec19a602751fd9dd3128e
david <ariedov@gmail.com>
2018-11-04 23:48:47 +0200

    adds game info converters

--- lib/tale_api/converters.dart
+++ lib/tale_api/converters.dart
@@ -9,28 +9,102 @@ Response<T> convertResponse<T>(dynamic json, Converter<T> converter) {
 }
 
 ThirdPartyLink convertThirdPartyLink(dynamic json) {
-  return ThirdPartyLink(
-    json["authorisation_page"] as String
-  );
+  return ThirdPartyLink(json["authorisation_page"] as String);
 }
 
 ThirdPartyStatus convertThirdPartyStatus(dynamic json) {
   return ThirdPartyStatus(
-    json["next_url"] as String,
-    json["account_id"] as int,
-    json["account_name"] as String,
-    json["session_expire_at"] as double,
-    json["state"] as int
-  );
+      json["next_url"] as String,
+      json["account_id"] as int,
+      json["account_name"] as String,
+      json["session_expire_at"] as double,
+      json["state"] as int);
 }
 
 ApiInfo convertApiInfo(dynamic json) {
   return ApiInfo(
-    json["static_content"] as String,
-    json["game_version"] as String,
-    json["turn_delta"] as int,
-    json["account_id"] as int,
-    json["account_name"] as String
+      json["static_content"] as String,
+      json["game_version"] as String,
+      json["turn_delta"] as int,
+      json["account_id"] as int,
+      json["account_name"] as String);
+}
+
+GameInfo convertGameInfo(dynamic json) {
+  return GameInfo(
+    json["mode"] as String,
+    convertTurn(json["turn"]),
+    json["game_state"] as int,
+    json["map_version"] as int,
+    convertAccount(json["account"]),
+    convertAccount(json["enemy"]),
+  );
+}
+
+Turn convertTurn(dynamic json) {
+  return Turn(json["number"] as int, json["verbose_date"] as String,
+      json["verbose_time"] as String);
+}
+
+Account convertAccount(dynamic json) {
+  return Account(
+      json["new_messages"] as int,
+      json["id"] as int,
+      json["last_visit"] as int,
+      json["is_own"] as bool,
+      json["is_old"] as bool,
+      convertHero(json["hero"]),
+      json["in_pvp_queue"] as bool,
+      json["energy"] as int);
+}
+
+Hero convertHero(dynamic json) {
+  return Hero(
+    json["patch_turn"] as int,
+    convertCompanion(json["companion"]),
+    convertBase(json["base"]),
+    convertSecondary(json["secondary"]),
+    json["diary"] as String,
+    json["messages"] as List<List<dynamic>>,
+  );
+}
+
+Companion convertCompanion(dynamic json) {
+  return Companion(
+    json["type"] as int,
+    json["name"] as String,
+    json["health"] as int,
+    json["max_health"] as int,
+    json["experience"] as int,
+    json["experience_to_level"] as int,
+    json["coherence"] as int,
+    json["real_coherence"] as int,
+  );
+}
+
+Base convertBase(dynamic json) {
+  return Base(
+    json["experience"] as int,
+    json["race"] as int,
+    json["health"] as int,
+    json["name"] as String,
+    json["level"] as int,
+    json["gender"] as int,
+    json["experience_to_level"] as int,
+    json["max_health"] as int,
+    json["destiny_points"] as int,
+    json["money"] as int,
+    json["alive"] as bool,
+  );
+}
+
+Secondary convertSecondary(dynamic json) {
+  return Secondary(
+    json["max_bag_size"] as int,
+    json["power"] as List<int>,
+    json["move_speed"] as double,
+    json["loot_items_count"] as int,
+    json["initiative"] as double,
   );
 }
 
