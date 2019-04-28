9a80047fb0a25372d62e4b2c61dad9cfc53dca7a
david <ariedov@gmail.com>
2019-04-28 15:20:14 +0300

    adds docker compose for simpler build

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,6 +4,6 @@ int get port => Platform.environment['PORT'] == null ? 8080 : int.parse(Platform
 
 String get telegramWebhook => Platform.environment['WEBHOOK'] == null ? "https://epictale-telegram.herokuapp.com/" : Platform.environment['WEBHOOK'];
 
-String get mongodbUri => Platform.environment['MONGO'] == null ? "mongodb://heroku_2l2n9pbx:dsghsn4mhu58rtqan2ap8hp5af@ds247223.mlab.com:47223/heroku_2l2n9pbx" : Platform.environment['MONGO'];
+String get mongodbUri => Platform.environment['MONGO'] == null ? "mongodb://mongo:27017" : Platform.environment['MONGO'];
 
 String get token => "663762224:AAEatW0mX8svEAZdgGpMOdGJZYXKIasONNc";
