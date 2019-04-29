262c504926c4b711c77d5978e803958363e73323
david <ariedov@gmail.com>
2019-04-29 23:18:40 +0300

    update token

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,8 +2,8 @@ import 'dart:io';
 
 int get port => Platform.environment['PORT'] == null ? 8080 : int.parse(Platform.environment['PORT']);
 
-String get telegramWebhook => Platform.environment['WEBHOOK'] == null ? "https://epictale.dleibovych.com" : Platform.environment['WEBHOOK'];
+String get telegramWebhook => Platform.environment['WEBHOOK'] == null ? "https://epictale.dleibovych.com/" : Platform.environment['WEBHOOK'];
 
 String get mongodbUri => Platform.environment['MONGO'] == null ? "mongodb://mongo:27017" : Platform.environment['MONGO'];
 
-String get token => "663762224:AAEatW0mX8svEAZdgGpMOdGJZYXKIasONNc";
+String get token => "663762224:AAGYa7Hvu-s2aCANh08wwBg7LL17QvUkcno";
