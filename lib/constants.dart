39e5630f1d7f165bd5d54e1042f3bbcef39db282
david <ariedov@gmail.com>
2019-04-29 15:26:43 +0300

    changes port

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 int get port => Platform.environment['PORT'] == null ? 8080 : int.parse(Platform.environment['PORT']);
 
-String get telegramWebhook => Platform.environment['WEBHOOK'] == null ? "https://epictale-telegram.herokuapp.com/" : Platform.environment['WEBHOOK'];
+String get telegramWebhook => Platform.environment['WEBHOOK'] == null ? "http://159.89.5.167:7070" : Platform.environment['WEBHOOK'];
 
 String get mongodbUri => Platform.environment['MONGO'] == null ? "mongodb://mongo:27017" : Platform.environment['MONGO'];
 
